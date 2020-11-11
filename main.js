bf2dc8ddc99665e5e222f7a926e97ab9dce6bcde
Maurizio <maurizio.faedda1@gmail.com>
2020-11-11 12:01:32 +0100

    eliminare l'item dalla lista

--- main.js
+++ main.js
@@ -12,8 +12,20 @@ var app = new Vue ({
     methods: {
 
         add_new_item(item) {
+            // aggiungo l'item in list e di coseguenza viene stampata grazie al v-for nell'html
             this.list.push(this.item);
+
+            // dopo che l'item viene aggiunto assegno a item un valore vuoto in modo da pulire l'input
             this.item = ""
+        },
+
+        remove_item(index) {
+            this.list.splice(this.index);
         }
+
+        // deleteItem: function(item) {
+        //     this.list.splice(this.list.indexOf(this.item));
+        // }
+
     }
 })
